b085a598ccf7f11348f936425dc9908914459cab
IvanLuquez <ivan961109@gmail.com>
2026-04-16 17:35:00 -0500

    Implementacion controlador y rutas para crud basico de api.

--- controller/pacientController.js
+++ controller/pacientController.js
@@ -19,6 +19,66 @@ exports.selectPacient = async (req, res) => {
         res.json(pacient);
     } catch (error) {
         console.log("Error al consultar los pacientes.");
+        res.status(500).send("Error al consultar.")
+    }
+}
 
+exports.updatePacient = async (req, res) => {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    try {
+        const pacientUpdated = await Pacient.findByIdAndUpdate(
+            id, 
+            updateData, 
+            { 
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!pacientUpdated) {
+            return res.status(404).json({ message: "Paciente no encontrado" });
+        }
+
+        res.status(200).json(pacientUpdated);
+    } catch (error) {
+        console.log("Error al actualizar el paciente");
+        res.status(500).json({ message: "Error al actualizar", error: error.message });
+    }
+};
+
+exports.findPacient= async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const pacientUpdated = await Pacient.findById( id );
+
+        if (!pacientUpdated) {
+            return res.status(404).json({ message: "Paciente no encontrado" });
+        }
+
+        res.status(200).json(pacientUpdated);
+    } catch (error) {
+        console.log("Error al consultar el paciente");
+        res.status(500).json({ message: "Error al consultar", error: error.message });
+    }
+}
+
+exports.deletePacient= async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const pacientUpdated = await Pacient.findById( id );
+
+        if (!pacientUpdated) {
+            return res.status(404).json({ message: "Paciente no encontrado" });
+        }
+        await Pacient.findByIdAndDelete(id);
+        res.status(200).json({response: "Paciente eliminado exitosamente."});
+    
+    } catch (error) {
+        console.log("Error al eliminar el paciente");
+        res.status(500).json({ message: "Error al eliminar", error: error.message });
     }
 }
--- routes/ClinicPaciente.js
+++ routes/ClinicPaciente.js
@@ -4,6 +4,8 @@ const pacienteController = require('../controller/pacientController');
 
 router.post('/', pacienteController.addPacient);
 router.get('/', pacienteController.selectPacient);
-
+router.put('/:id', pacienteController.updatePacient);
+router.get('/:id', pacienteController.findPacient);
+router.delete('/:id', pacienteController.deletePacient);
 
 module.exports = router;
